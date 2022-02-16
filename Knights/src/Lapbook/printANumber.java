c80c25656d8bcb1fdb9528721f2fd11b77464e13
Rifay Ahamed <Rifay Ahamed@RifayAhamed-PC>
2022-02-16 18:30:25 +0530

    k

---
+++ Knights/src/Lapbook/printANumber.java
@@ -0,0 +1,19 @@
+package Lapbook;
+import java.util.Scanner;
+public class printANumber {
+ public static void main(String[] args) {  
+	 
+	 Scanner scan=new Scanner(System.in);
+System.out.println("Enter value for i..:");
+
+	int i=scan.nextInt();
+	
+	  System.out.println("Enter value for j..:");
+	  
+	      int j=scan.nextInt();
+	      int mul=i*j;
+	      System.out.println("The Multiplication of i and j is..:"+mul);
+	      
+	}
+ 
+}
